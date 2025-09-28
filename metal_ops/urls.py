c261ed0ced805e74413930b3917934ca3e9277f5
Luladusilva <gabriela1.8.wib@gmail.com>
2025-09-28 13:25:17 -0500

    fix: delete and edit for cliente y tarea in Views y Urls

--- metal_ops/urls.py
+++ metal_ops/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    CrearSedeView, CrearClienteView, CrearOrdenTrabajoView, ListarOrdenTrabajoView, EditarOrdenTrabajoView,
+    CrearSedeView, ListarSedeView, CrearClienteView, CrearOrdenTrabajoView, ListarOrdenTrabajoView, EditarOrdenTrabajoView,
     EliminarOrdenTrabajoView, ListarMaquinaView, CrearServicioView, ListarClienteView, EditarClienteView,
     EliminarClienteView,
     CrearMaquinaView, CrearTareaView, ListarTareaView, EditarTareaView,
@@ -9,11 +9,11 @@ from .views import (
 
 urlpatterns = [
     path('sedes/crear/', CrearSedeView.as_view(), name='crear-sede'),
-    path('sedes/lista/', ListarClienteView.as_view(), name='lista-sede'),
+    path('sedes/lista/', ListarSedeView.as_view(), name='lista-sede'),
     path('clientes/crear/', CrearClienteView.as_view(), name='crear-cliente'),
     path('clientes/lista/', ListarClienteView.as_view(), name='lista-cliente'),
-    path('clientes/editar/', EditarClienteView.as_view(), name='editar-cliente'),
-    path('clientes/eliminar/', EliminarClienteView.as_view(), name='eliminar-cliente'),
+    path('clientes/editar/<int:id_cliente>', EditarClienteView.as_view(), name='editar-cliente'),
+    path('clientes/eliminar/<int:id_cliente>', EliminarClienteView.as_view(), name='eliminar-cliente'),
     path('ordenes/crear/', CrearOrdenTrabajoView.as_view(), name='crear-orden'),
     path('ordenes/lista/', ListarOrdenTrabajoView.as_view(), name='lista-orden'),
     path('ordenes/editar/<int:id_ot>', EditarOrdenTrabajoView.as_view(), name='editar-orden'),
@@ -23,7 +23,7 @@ urlpatterns = [
     path('servicios/crear/', CrearServicioView.as_view(), name='crear-servicio'),
     path('tareas/crear/', CrearTareaView.as_view(), name='crear-tarea'),
     path('tareas/lista/', ListarTareaView.as_view(), name='lista-tarea'),
-    path('tareas/editar/', EditarTareaView.as_view(), name='editar-tarea'),
-    path('tareas/eliminar/', EliminarTareaView.as_view(), name='eliminar-tarea'),
+    path('tareas/editar/<int:id_tarea>', EditarTareaView.as_view(), name='editar-tarea'),
+    path('tareas/eliminar/<int:id_tarea>', EliminarTareaView.as_view(), name='eliminar-tarea'),
     
 ]   
