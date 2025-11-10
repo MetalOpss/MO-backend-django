45bdc9f95fb13c040aab2c8da643c0a55262f383
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2025-11-09 23:47:10 -0500

    feat: cambio de configuraacion de permisos en backedn + proteccion de notificacion en django

--- metal_ops/permissions.py
+++ metal_ops/permissions.py
@@ -23,3 +23,12 @@ class IsAtencion(BasePermission):
     def has_permission(self, request, view):
         role = request.auth.get("rol") if request.auth else None  
         return role == "ATENCION"
+    
+def ORPermission(*permissions):
+    """
+    Devuelve una clase de permiso que hace OR lógico entre las que recibe.
+    """
+    class _ORPermission(BasePermission):
+        def has_permission(self, request, view):
+            return any(p().has_permission(request, view) for p in permissions)
+    return _ORPermission
