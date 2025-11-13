7d350fb805715a41eda15be5048eaa340385cffa
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2025-11-13 18:14:29 -0500

    add permisions to OT backend

--- metal_ops/views/orden_trabajo.py
+++ metal_ops/views/orden_trabajo.py
@@ -1,23 +1,24 @@
 from rest_framework import generics
 from metal_ops.models import OrdenTrabajo
 from metal_ops.serializers import OrdenTrabajoSerializer
-from metal_ops.permissions import IsAtencion
+from metal_ops.permissions import IsAtencion, IsAdmin, IsOperario, IsPlanner, ORPermission
 
 class CrearOrdenTrabajoView(generics.CreateAPIView):
     queryset = OrdenTrabajo.objects.all()
     serializer_class = OrdenTrabajoSerializer
     permission_classes = [IsAtencion]
 
+
 class ListarOrdenTrabajoView(generics.ListAPIView):
     queryset = OrdenTrabajo.objects.all()
     serializer_class = OrdenTrabajoSerializer
-    permission_classes = [IsAtencion]
+    permission_classes = [ORPermission(IsAtencion, IsAdmin, IsOperario, IsPlanner)]
     
 class EditarOrdenTrabajoView(generics.UpdateAPIView):
     queryset = OrdenTrabajo.objects.all()
     serializer_class = OrdenTrabajoSerializer
     lookup_field = "id_ot"
-    permission_classes = [IsAtencion]
+    permission_classes = [ORPermission(IsAtencion, IsAdmin, IsOperario, IsPlanner)]
 
 class EliminarOrdenTrabajoView(generics.DestroyAPIView):
     queryset = OrdenTrabajo.objects.all()
