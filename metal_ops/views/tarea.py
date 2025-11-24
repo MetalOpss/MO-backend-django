e811896fd4a9ba8091e9d1cc653dcf12a81c08bb
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2025-11-24 18:42:09 -0500

    feat: perimisos para admin

--- metal_ops/views/tarea.py
+++ metal_ops/views/tarea.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from metal_ops.models import Tarea
 from metal_ops.serializers import TareaSerializer
-from metal_ops.permissions import IsAtencion, ORPermission, IsPlanner, IsOperario
+from metal_ops.permissions import IsAtencion, ORPermission, IsPlanner, IsOperario, IsAdmin
 
 class CrearTareaView(generics.CreateAPIView):
     queryset = Tarea.objects.all()
@@ -11,7 +11,7 @@ class CrearTareaView(generics.CreateAPIView):
 class ListarTareaView(generics.ListAPIView):
     queryset = Tarea.objects.all()
     serializer_class = TareaSerializer
-    permission_classes = [ORPermission(IsPlanner, IsAtencion, IsOperario)]
+    permission_classes = [ORPermission(IsPlanner, IsAtencion, IsOperario, IsAdmin)]
 
 class EditarTareaView(generics.UpdateAPIView):
     queryset = Tarea.objects.all()
