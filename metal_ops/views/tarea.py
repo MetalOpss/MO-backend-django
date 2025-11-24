2748931d5b2f23493a14bfd35ee34750fd813a82
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2025-11-23 20:58:15 -0500

    cambios en permisos

--- metal_ops/views/tarea.py
+++ metal_ops/views/tarea.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from metal_ops.models import Tarea
 from metal_ops.serializers import TareaSerializer
-from metal_ops.permissions import IsAtencion, ORPermission, IsPlanner
+from metal_ops.permissions import IsAtencion, ORPermission, IsPlanner, IsOperario
 
 class CrearTareaView(generics.CreateAPIView):
     queryset = Tarea.objects.all()
@@ -11,13 +11,13 @@ class CrearTareaView(generics.CreateAPIView):
 class ListarTareaView(generics.ListAPIView):
     queryset = Tarea.objects.all()
     serializer_class = TareaSerializer
-    permission_classes = [ORPermission(IsPlanner, IsAtencion)]
+    permission_classes = [ORPermission(IsPlanner, IsAtencion, IsOperario)]
 
 class EditarTareaView(generics.UpdateAPIView):
     queryset = Tarea.objects.all()
     serializer_class = TareaSerializer
     lookup_field = "id_tarea"
-    permission_classes = [ORPermission(IsPlanner, IsAtencion)]
+    permission_classes = [ORPermission(IsPlanner, IsAtencion, IsOperario)]
 
 class EliminarTareaView(generics.DestroyAPIView):
     queryset = Tarea.objects.all()
